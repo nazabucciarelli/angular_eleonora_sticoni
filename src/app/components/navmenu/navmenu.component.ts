2c5e0676054356e8bea05c6f706ad2e199ba1c55
nazabucciarelli <bnazareno03@gmail.com>
2023-06-21 13:37:06 -0300

    fixing navbar

--- src/app/components/navmenu/navmenu.component.ts
+++ src/app/components/navmenu/navmenu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navmenu',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./navmenu.component.css']
 })
 export class NavmenuComponent {
+  
+  constructor(private elementRef: ElementRef) { }
+
+  ngOnInit() {
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+    const navbarToggler = this.elementRef.nativeElement.querySelector('.navbar-toggler');
+    const navbarCollapse = this.elementRef.nativeElement.querySelector('.navbar-collapse');
+
+    if (!navbarToggler.contains(clickedElement) && !navbarCollapse.contains(clickedElement) ) {
+      navbarCollapse.classList.remove('show');
+    }
+  }
+
+  closeNavbar() {
+    const navbarCollapse = this.elementRef.nativeElement.querySelector('.navbar-collapse');
+    navbarCollapse.classList.remove('show');
+  }
 
 }
