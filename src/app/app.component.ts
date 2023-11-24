34aebd35fcccb09ef836fff5b3ded76cf94f343b
Nazareno Bucciarelli <bnazareno03@gmail.com>
2023-11-23 21:03:57 -0300

    removing spinner, fixing scroll problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit, AfterContentChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterContentChecked{
+export class AppComponent implements OnInit{
   showLoader: boolean = true;
 
-  constructor(){}
-  
-  ngAfterContentChecked() {
-    this.showLoader = false;  
-  }
+  constructor(public router: Router){}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0); 
+      }
+  })
 
 }
-
+}
